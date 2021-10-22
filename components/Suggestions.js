707870d10835f2c5b0ee2217b9b5bb7b82cdc996
Mandar899 <mandarmanvi899@gmail.com>
2021-10-22 11:17:37 +0530

    🛠Fix Margin

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -12,7 +12,7 @@ function Suggestions() {
     setSuggestions(suggestions);
   }, []);
   return (
-    <div>
+    <div className="mt-4 ml-10">
       <div className="flex justify-between text-sm mb-5">
         <h3 className="text-sm font-bold tracking-tight text-gray-400">
           Suggestions for you

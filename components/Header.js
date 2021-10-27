5ed82d0691f7cd06e2e7a81df097e6914c475001
Mandar899 <mandarmanvi899@gmail.com>
2021-10-27 12:24:51 +0530

    🛠Fix Header

--- components/Header.js
+++ components/Header.js
@@ -20,7 +20,7 @@ function Header() {
 
   return (
     <div className="shadow-sm border-b bg-white sticky top-0 z-50">
-      <div className="flex justify-between bg-white max-w-6xl mx-5 lg:mx-auto cursor-pointer">
+      <div className="flex justify-between bg-white max-w-6xl mx-5 lg:mx-auto cursor-pointer py-5 md:py-0">
         {/* left - instagram logo */}
         <div
           className="relative w-24 hidden lg:inline-grid cursor-pointer"
@@ -44,7 +44,7 @@ function Header() {
         </div>
 
         {/* center - search bar */}
-        <div className="max-w-xs mx-auto">
+        <div className="max-w-xs mx-auto hidden md:block">
           <div className="relative mt-1 p-3 rounded-md">
             <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
               <SearchIcon className="h-5 w-5 text-gray-500" />
@@ -60,7 +60,7 @@ function Header() {
         {/* right - buttons */}
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon className="navBtn " onClick={() => router.push("/")} />
-          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          {/* <MenuIcon className="h-6 md:hidden cursor-pointer" /> */}
 
           {session ? (
             <>

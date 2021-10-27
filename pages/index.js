cd3467059026cbdfe2440a1efeb7982b93ba9fd1
Mandar899 <mandarmanvi899@gmail.com>
2021-10-27 18:54:35 +0530

    🛠Remove toastify from homepage

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,11 @@
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import { useEffect } from "react";
 import Feed from "../components/Feed";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   const { data: session } = useSession();
-
-  // console.log(session);
-  useEffect(() => {
-    !session &&
-      toast("Sign in to view posts.", {
-        position: "top-right",
-        autoClose: 4000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-  }, []);
 
   return (
     <div className="bg-[#FAFAFA] h-screen overflow-y-scroll scrollbar-hide">
@@ -39,18 +22,6 @@ export default function Home() {
 
       {/* Modal */}
       <Modal />
-
-      <ToastContainer
-        position="top-right"
-        autoClose={4000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
     </div>
   );
 }
